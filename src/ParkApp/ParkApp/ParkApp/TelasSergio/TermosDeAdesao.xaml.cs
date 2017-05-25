a899d0f6d5923ce3d8c5db52596265354d59a09c
Usuário <Usurio@DESKTOP-DGJSGQF>
2017-05-25 18:09:48 -0300

    Termos de Uso - Aceitar

---
+++ src/ParkApp/ParkApp/ParkApp/TelasSergio/TermosDeAdesao.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ParkApp.TelasSergio
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TermosDeAdesao : ContentPage
+    {
+        public TermosDeAdesao()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        async void btAceitar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+            await Navigation.PushAsync(new MasterDetail.MasterDetailPage1(),true);
+            
+        }
+
+        async void btRejeitar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TelasSergio.Login(), true);
+        }
+    }
+}
