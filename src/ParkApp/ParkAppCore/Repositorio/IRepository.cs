85bff2b754bbabc05d1a343ca21268e18ddf58e9
Alessandro Binhara <binhara@gmail.com>
2017-05-26 12:32:22 -0300

    Add novo projeto de teste

---
+++ src/ParkApp/ParkAppCore/Repositorio/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ParkAppCore.Repositorio
+{
+    public interface IRepository<T>
+    {
+        bool Insert(T obj);
+        bool Remove(T obj);
+        bool Update(T obj);
+        IList<T> Find();
+    }
+}
