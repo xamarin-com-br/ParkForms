85942f5c4f55bc8472bba1aa224c5b471e9a767d
Usuário <Usurio@DESKTOP-DGJSGQF>
2017-05-26 12:28:00 -0300

    tumtumtum

--- src/ParkApp/ParkAppCore/Models/Aluguel.cs
+++ src/ParkApp/ParkAppCore/Models/Aluguel.cs
@@ -7,13 +7,10 @@ namespace ParkAppCore.Models
     public class Aluguel
     {
         public int ID { get; set; }
-
         public DateTime Inicio { get; set; }
-
         public DateTime Fim { get; set; }
-
         public int VagaID { get; set; }
-
-
+        public decimal Ecoins { get; set; }
+        public decimal EcoinsUtilizados { get; set; }
     }
 }
