ba61e4142cb5857724859317d961962d5cf64919
Alessandro Binhara <binhara@gmail.com>
2017-05-26 09:11:00 -0300

    Merger Feito… e compilando , verificar se tudo esta funcionado

--- src/ParkApp/ParkApp/ParkApp/Telas/Login.cs
+++ src/ParkApp/ParkApp/ParkApp/Telas/Login.cs
@@ -7,46 +7,48 @@ using Xamarin.Forms;
 
 namespace ParkApp.Telas
 {
-    public class Login : ContentPage
-    {
-        Button buttonMessage;
-        Label aviso;
-        int clickTotal=1;
+	public class Login : ContentPage
+	{
+		Button buttonMessage;
+		Label aviso;
+		int clickTotal = 1;
 
-        public Login()
-        {
-            buttonMessage = new Button {
-                Text  = "Mensagem"
+		public Login()
+		{
+			buttonMessage = new Button
+			{
+				Text = "Mensagem"
 
-            };
-            buttonMessage.Clicked += OnButtonClicked;
+			};
+			buttonMessage.Clicked += OnButtonClicked;
 
-            aviso = new Label
-            {
-                Text = "Criar conta"
-            };
+			aviso = new Label
+			{
+				Text = "Criar conta"
+			};
 
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "Nome do usuário!" },
-                    new Entry { },
-                    new Label {Text = "Senha" },
-                    new Entry { },
-                    new Button {Text = "Login with E-APP" },
-                    new Button { Text = "Login with Facebook" },
-                    new Button { Text = "Login with Twitter" },
-                     aviso
-                }
-            };
-        }
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Nome do usuário!" },
+					new Entry { },
+					new Label {Text = "Senha" },
+					new Entry { },
+					new Button {Text = "Login with E-APP" },
+					new Button { Text = "Login with Facebook" },
+					new Button { Text = "Login with Twitter" },
+					 aviso
+				}
+			};
+		}
 
-        void OnButtonClicked(object sender, EventArgs e)
-        {
-            
-            clickTotal += 1;
-           aviso.Text = String.Format("click{0}",
-                                       clickTotal);
-        }
-    }
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+
+			clickTotal += 1;
+			aviso.Text = String.Format("click{0}",
+										clickTotal);
+		}
+	}
 }
+
