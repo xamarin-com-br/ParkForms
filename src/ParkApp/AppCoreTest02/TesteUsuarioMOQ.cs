04d3866cc7a6705d731d016f57423236c1be15f4
Alessandro Binhara <binhara@gmail.com>
2017-05-26 14:55:55 -0300

    no message

--- src/ParkApp/AppCoreTest02/TesteUsuarioMOQ.cs
+++ src/ParkApp/AppCoreTest02/TesteUsuarioMOQ.cs
@@ -11,6 +11,8 @@ namespace AppCoreTest02
     [TestFixture()]
     public class TesteUsuarioMOQ
     {
+        public IRepository<Usuario> UsuariosMock;
+
         [TestFixtureSetUp]
 		public void TestCaseSetup()
 		{
@@ -18,8 +20,8 @@ namespace AppCoreTest02
             IList<Usuario> usuarios = new List<Usuario>
             {
                 new Usuario(){ ID = 1, Nome = "Jose", Senha = "1234", Login = "l1" } ,
-                new Usuario(){ ID = 2, Nome = "Jose2", Senha = "1234", Login = "l2" },
-                new Usuario(){ ID = 3, Nome = "Jose3", Senha = "1234", Login = "l3" } 
+                new Usuario(){ ID = 2, Nome = "Jose2", Senha = "2", Login = "l2" },
+                new Usuario(){ ID = 3, Nome = "Jose3", Senha = "3", Login = "l3" } 
             };
 
 
@@ -30,7 +32,7 @@ namespace AppCoreTest02
 			//mockDeviceRepository.Setup(mr => mr.Find( It.IsAny<int>() )).Returns((int i) => devices.Where(x => x.ID == i).Single());
 
             //mockDeviceRepository.Setup( i => i.Insert(It.IsAny<Usuario>() ) ).Returns{ usuarios.Add(It.IsAny<Usuario>());} ;
-
+            this.UsuariosMock = mockDeviceRepository.Object;
 		}
 
 
@@ -47,11 +49,24 @@ namespace AppCoreTest02
 		[Test()]
 		public void TestFind()
 		{
+            var lista =  UsuariosMock.Find();
+            Assert.AreEqual(lista.Count, 3);
+
 		}
 
 		[Test()]
 		public void TestFindID()
 		{
 		}
+
+		[Test()]
+		public void TestLoginAutenticacao()
+		{
+            var lista = UsuariosMock.Find();
+            Assert.AreEqual(lista[0].Autenticar("jose", "1234"),true);
+
+		}
+
+
     }
 }
